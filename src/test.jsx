76c34512b687824ece11048b362372ba014030bb
Andy Smith <asmith@ScottLogic.com>
2019-04-30 17:09:24 +0100

    Added test.jsx

---
+++ src/test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const name = 'Jim Beam';
+const formatName = (user) => user.firstName + ' ' + user.lastName;
+
+const user = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const element = <h1>Hello, {formatName(user)}</h1>
+
+function Test() {
+    return element;
+};
+
+export default Test;
