325d885733edad20693b8d2b7d85a4a766183f51
Andy Smith <andysmithred@yahoo.com>
2019-05-17 14:09:25 +0100

    Fix footer file name

--- src/components/app/footer/Footerx.jsx
+++ src/components/app/footer/Footerx.jsx
@@ -5,5 +5,6 @@ import './Footer.scss';
 export const Footer = () => (
     <div className="Footer">
         <a href="https://www.scottlogic.com/" className="sl-logo theme-logo" />
+        <div></div>
     </div>
 );
