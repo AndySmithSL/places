6e408b0ad025096854ba3a7fb5e11b5543396f04
Andy Smith <asmith@ScottLogic.com>
2019-07-23 17:51:55 +0100

    New icons

--- src/components/app/utils/ImageDetails.js
+++ src/components/app/utils/ImageDetails.js
@@ -38,61 +38,102 @@ export const ImageDetails = {
             name: "picture",
             image: "picture.png"
         },
-
-
-
-
         {
-            name: "aircraft",
-            image: "aircraft.png"
+            name: "road",
+            image: "road.png"
         },
         {
-            name: "airport",
-            image: "airport.png"
-        },
-        
-        {
-            name: "city-hall",
-            image: "city-hall.png"
-        },
-        
-        {
-            name: "forward",
-            image: "forward.png"
-        },
-       
-        
-        
-        {
-            name: "image",
-            image: "image.png"
+            name: "info-squared",
+            image: "info-squared.png"
         },
         {
             name: "information",
             image: "info.png"
         },
         {
+            name: "world-map",
+            image: "world-map.png"
+        },
+        {
+            name: "forward",
+            image: "forward.png"
+        },
+        {
+            name: "image",
+            image: "image.png"
+        },
+        {
             name: "journey",
             image: "journey.png"
         },
+        {
+            name: "route",
+            image: "route.png"
+        },
+
+        //Place Groups
+
+        {   // Airport (small)
+            name: "aircraft",
+            image: "aircraft.png"
+        },
+        {   // International Airport
+            name: "airport",
+            image: "airport.png"
+        },
+        {   // Atoll
+            name: "beach",
+            image: "beach.png"
+        },
+
+        {   // Cave
+            name: "cave",
+            image: "cave.png"
+        },
+
+        {   // Reef
+            name: "coral",
+            image: "coral.png"
+        },
+
+        {   // Natural Wonders
+            name: "grand-canyon",
+            image: "grand-canyon.png"
+        },
+
+
+
+
+
+
+        
+
+
+
+
+
+
+        
+        
+        
+        {
+            name: "city-hall",
+            image: "city-hall.png"
+        },
+        
+        
+       
+        
+        
         {
             name: "map",
             image: "map.png"
         },
        
         
-        {
-            name: "road",
-            image: "road.png"
-        },
-        {
-            name: "route",
-            image: "route.png"
-        },
-        {
-            name: "world-map",
-            image: "world-map.png"
-        }
+        
+        
+        
     ]  
 };
 
