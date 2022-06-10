e1174da79ac1d9564d864d49f5ad0431928c4ba9
Andy Smith <andysmithred@yahoo.com>
2022-06-10 18:04:02 +0100

    More cleanups - Prettier

--- src/components/app/main/content/continents/ContinentMap.jsx
+++ src/components/app/main/content/continents/ContinentMap.jsx
@@ -1,48 +1,46 @@
-import React from 'react';
-import Chart from 'react-google-charts';
+//import React from "react";
+import Chart from "react-google-charts";
+import { DetailsHeader } from "../common/DetailsHeader";
+import "./ContinentMap.scss";
 
-import { DetailsHeader } from '../common/DetailsHeader';
+export const ContinentMap = (props) => {
+  const options = {
+    region: props.item && props.item.code,
+    resolution: "countries",
+    keepAspectRatio: false,
+    displayMode: "auto",
+    enableRegionInteractivity: true,
+    backgroundColor: "#282c34",
+    datalessRegionColor: "#282c34",
+    defaultColor: "floralwhite",
+    tooltip: {
+      textStyle: {
+        color: "black",
+        fontName: "Segoe UI",
+        fontSize: 18,
+        bold: true,
+      },
+    },
+  };
 
-import './ContinentMap.scss';
+  let data = [];
+  data.push(["Country", "Name"]);
 
-export const ContinentMap = props => {
+  props.item.totalTerritories.forEach((x) => {
+    let item = [x.isocode, x.name];
+    //item.push(x.isocode, x.name);
+    data.push(item);
+  });
 
-    const options = {
-        region: props.item && props.item.code,
-        resolution: 'countries',
-        keepAspectRatio: false,
-        displayMode: 'auto',
-        enableRegionInteractivity: true,
-        backgroundColor: '#282c34',
-        datalessRegionColor: '#282c34',
-        defaultColor: 'floralwhite',
-        tooltip: {
-            textStyle: {
-                color: 'black',
-                fontName: 'Roboto',
-                fontSize: 18,
-                bold: false
-            }
-        }
-    }
-
-    let data = [];
-    data.push(['Country', 'Name']);
-
-    props.item.totalTerritories.forEach(x => {
-        let item = [];
-        item.push(x.isocode, x.name);
-        data.push(item);
-    });
-
-    return (
-        <div className='ContinentMap' >
-            <DetailsHeader icon={props.icon} label={props.label} />
-            <Chart className='chart' 
-                    chartType='GeoChart' 
-                    data={data}              
-                    options={options}
-                />
-        </div>
-    );
-}
+  return (
+    <div className="ContinentMap">
+      <DetailsHeader icon={props.icon} label={props.label} />
+      <Chart
+        className="chart"
+        chartType="GeoChart"
+        data={data}
+        options={options}
+      />
+    </div>
+  );
+};
